639e75637400487ce689aeb13f1ff0ed3a707a62
jorgemoniz <jorgemoniz@live.com>
2017-03-08 20:57:03 +0100

    Revert "Ajustes_Desarrollo_Implementación de Modelo / Parser / tableView Controller / XIB y mejoras de recarga de la TableView _ 080317 _ 2055"
    This reverts commit 27459e299a33ddbe0b512d6f47d3565e0a045245.

--- APP_BaresdeMadrid/BMListaNoticiasTableViewController.swift
+++ APP_BaresdeMadrid/BMListaNoticiasTableViewController.swift
@@ -17,6 +17,7 @@ class BMListaNoticiasTableViewController: UITableViewController {
     var arrayNoticias : [BMNoticiasModel] = []
     
     //MARK: - IBOutlets
+    
     @IBOutlet weak var menuButton: UIBarButtonItem!
     @IBOutlet weak var extraMenuButton: UIBarButtonItem!
 
@@ -28,8 +29,7 @@ class BMListaNoticiasTableViewController: UITableViewController {
         
         // REGISTRAMOS EL XIB
         tableView.register(UINib(nibName: "BMNoticiaCustomCell", bundle: nil), forCellReuseIdentifier: "NoticiaCustomCell")
-        
-        // CREAMOS EL MENÚ
+
         if revealViewController() != nil {
             menuButton.target = revealViewController()
             menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
